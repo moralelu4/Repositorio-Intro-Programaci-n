59a10cfa8d3886ed086354a2c8b487c8310e2ef0
DanAraya1998 <danarayator@gmail.com>
2024-03-09 17:44:44 -0600

    Incorporada división de trabajo.

---
+++ Avance1/Código_Avance1.py
@@ -0,0 +1,7 @@
+### División de partes:
+### 1. Menú: Mariana
+### 2. Entrada de registro de usuarios: Fabián
+### 3. Inicio de sesión de usuario: Anderson
+### 4. Control de casas: Luis
+### 5. Entrada de habitaciones: Daniel
+### 6. Control de variables: Jordi
