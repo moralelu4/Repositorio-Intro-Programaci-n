ccbfda636792f9206a13e59f81b0f70dccd46d86
ProgrammerN18 <jormgdi18@gmail.com>
2024-03-09 18:26:31 -0600

    Menú incorporaddo otra vez

--- Avance1/Código_Avance1.py
+++ Avance1/Código_Avance1.py
@@ -4,3 +4,17 @@
 ### 4. Control de casas: Luis
 ### 5. Entrada de habitaciones: Daniel
 ### 6. Control de variables: Mariana
+
+while True:
+    opcion = input("\nBienvenido a la aplicación Smart Home\n1. Iniciar sesión\n2. Registrarse\n3. Salir\nSeleccione una opción: ")
+
+    if opcion == "1":
+        print("Opción de inicio de sesión")
+    elif opcion == "2":
+        print("Opción de registro de usuario")
+        break
+    elif opcion == "3":
+        print("Saliendo de la aplicación.")
+        break
+    else:
+        print("Opción inválida. Inténtelo de nuevo.")
