eced3e6c02e268e7ea1b3239f2ba3380cfc3ba47
fabso2001 <fsandoval20187@ufide.ac.cr>
2024-03-10 12:13:02 -0600

    Registro de usuarios implementado

--- Avance1/Código_Avance1.py
+++ Avance1/Código_Avance1.py
@@ -1,5 +1,4 @@
 
-### 2. Entrada de registro de usuarios: Fabián
 ### 3. Inicio de sesión de usuario: Anderson
 ### 4. Control de casas: Luis
 ### 5. Entrada de habitaciones: Daniel
@@ -13,6 +12,29 @@ while True:
     elif opcion == "2":
         print("Opción de registro de usuario")
         break
+    elif opcion == "3":
+        print("Saliendo de la aplicación.")
+        break
+    else:
+        print("Opción inválida. Inténtelo de nuevo.")
+        
+correo = ""
+while True:
+    if correo == "":
+        nombre = input("Ingrese su nombre: ")
+        correo = input("Ingrese un correo electrónico: ")
+        pin = input("Ingrese un PIN: ")
+        print("Usuario registrado exitosamente.")
+
+    opcion = input("\nBienvenido a la aplicación Smart Home\n1. Iniciar sesión\n2. Registrarse\n3. Salir\nSeleccione una opción: ")
+
+    if opcion == "1":
+        print("Opción de inicio de sesión.")
+    elif opcion == "2":
+        nombre = input("Ingrese su nombre: ")
+        correo = input("Ingrese su correo electrónico: ")
+        pin = input("Ingrese un PIN: ")
+        print("Usuario registrado exitosamente.")
     elif opcion == "3":
         print("Saliendo de la aplicación.")
         break
