dc36932083b02fee34f70a2dc5f9e343043131f1
andernc <158122860+andernc@users.noreply.github.com>
2024-03-10 21:29:09 +0000

    inicio de sesion de usuario

--- Avance1/Código_Avance1.py
+++ Avance1/Código_Avance1.py
@@ -40,3 +40,38 @@ while True:
         break
     else:
         print("Opción inválida. Inténtelo de nuevo.")
+correo = ""
+nombre_casa = ""
+casas = ""
+contador_casas = 0
+while True:
+    if correo == "":
+        nombre = input("Ingrese su nombre: ")
+        correo = input("Ingrese un correo electrónico: ")
+        pin = input("Ingrese un PIN: ")
+        print("Usuario registrado exitosamente.")
+
+    opcion = input("\nBienvenido a la aplicación Smart Home\n1. Iniciar sesión\n2. Registrarse\n3. Salir\nSeleccione una opción: ")
+    if opcion == "1":
+        correo_login = input("Ingrese su correo electrónico: ")
+        pin_login = input("Ingrese su PIN: ")
+        if correo == correo_login and pin == pin_login:
+            print("Inicio de sesión exitoso.")
+            print("Entrada a casas.")
+        else:
+            print("Usuario no encontrado. Por favor, regístrese.")
+    if opcion == 2:
+        nombre = input("Ingrese su nombre: ")
+        correo = input("Ingrese su correo electrónico: ")
+        pin = input("Ingrese un PIN: ")
+        print("Usuario registrado exitosamente.")
+    elif opcion == "3":
+        print("Saliendo de la aplicación.")
+        break
+    else:
+        print("Opción inválida. Inténtelo de nuevo.")
+        
+
+ ### TO-DO LIST: ###
+        # 1. Estaría interesante poder validar los correos (de momento, cualquier string pasa como correo).
+        # 2. Orden en el caso de las casas: 1. Casa. 2. Habitación. 3. (ya para siguiente entrega) dispositivos (Párrafos 6 y 7 de las instrucciones). Falta la parte de registrar la habitación.
